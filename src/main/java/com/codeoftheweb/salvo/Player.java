6880afe1bd0ab6a0d912af0daa34d37cb73e2c5d
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-12-20 10:49:17 +0100

    Add Player entity

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,49 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String userName;
+
+    public Player() { }
+
+    public Player(Long id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
