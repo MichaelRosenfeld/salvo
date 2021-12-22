9154b97659ae42c718348e449b2fd119b1f1c5c4
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-12-22 12:11:23 +0100

    Refactor file structure

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -1,5 +1,7 @@
 package com.codeoftheweb.salvo;
 
+import com.codeoftheweb.salvo.domain.Player;
+import com.codeoftheweb.salvo.repositories.PlayerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
