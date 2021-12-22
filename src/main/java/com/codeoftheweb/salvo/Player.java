b37ed50f1f6377ebb956af5db16e9609551a9d6c
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-12-22 11:51:28 +0100

    Add index page and code to retrieve raw json.

--- src/main/java/com/codeoftheweb/salvo/Player.java
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -1,7 +1,6 @@
 package com.codeoftheweb.salvo;
 
 import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
